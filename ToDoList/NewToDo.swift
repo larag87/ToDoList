7d21933f2a304e9d9270040812753c174544f43c
Lara <laragandre@gmail.com>
2025-08-08 13:25:59 -0400

    Added a New Swift UI To Do View

---
+++ ToDoList/NewToDo.swift
@@ -0,0 +1,38 @@
+//
+//  NewToDo.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDo: View {
+    var body: some View {
+        VStack {
+            Text("Task title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }//end of toggle
+            Button {
+            }label: {
+                Text("Save")
+            }
+            
+            
+        }//end of Vstack
+        .padding()
+        }
+    }
+
+
+#Preview {
+    NewToDo()
+}
